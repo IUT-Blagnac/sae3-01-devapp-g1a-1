d15aeb17ae0cf2e1616e38f7c55f2900554eb107
IUT BLAGNAC <noreply@univ-tlse2.fr>
2022-12-14 12:27:57 +0100

    Mise à jour appli IoT

--- Applications/Python/SAEIOT.py
+++ Applications/Python/SAEIOT.py
@@ -5,6 +5,7 @@ import time
 import json
 import xmltodict
 import datetime
+import threading
 
 with open("config.xml") as fd:
     config = xmltodict.parse(fd.read())
@@ -15,6 +16,11 @@ toDraw = config["toDraw"]
 max = config["max"]
 min = config["min"]
 timeSleep = int(config["timeSleep"])
+
+dataTable = {}
+
+for v in toDraw:
+    dataTable[v] = 0
 
 client = mqtt.Client("SAE_G1A-1")
 
@@ -29,21 +35,36 @@ def on_message(client, userdata, message):
     data = json.loads(message.payload.decode("utf-8"))
     data = data["object"]
 
-    for test in toDraw:
-        if toDraw[test] == "True":
-            out = os.open(str(config["outFolder"]) + "/" + str(test)+".dce", os.O_CREAT | os.O_TRUNC | os.O_WRONLY)
-            
-            os.write(out, bytes(str(data[test]), 'utf-8'))
-            if data[test] >= int(max[test]) :
-                os.write(out, bytes("\nmax", 'utf-8'))
-            if data[test] <= int(min[test]) :
-                os.write(out, bytes("\nmin", 'utf-8'))
-            os.close(out)
+    for v in toDraw:
+        dataTable[v] = data[v]
 
     # print("message topic=",message.topic)
     # print("message qos=",message.qos)
     # print("message retain flag=",message.retain)
 
+
+def saveData() :
+    print("saving data")
+    for test in toDraw:
+        if toDraw[test] == "True":
+            out = os.open(str(config["outFolder"]) + "/" + str(test)+".dce", os.O_CREAT | os.O_TRUNC | os.O_WRONLY)
+            os.write(out, bytes(str(dataTable[test]), 'utf-8'))
+            os.close(out)
+
+            if dataTable[test] >= int(max[test]) or dataTable[test] <= int(min[test]):
+                out = os.open(str(config["outFolder"]) + "/alerte" + str(test) + ".dce", os.O_CREAT | os.O_TRUNC | os.O_WRONLY)
+                os.write(out, bytes(str(dataTable[test]) + ":" + str(datetime.datetime.now()), 'utf-8'))
+                os.close(out)
+
+def set_interval(fonction, temps):
+    def func_wrapper():
+        set_interval(fonction, temps)
+        fonction()
+    t = threading.Timer(temps, func_wrapper)
+    t.start()
+    return t
+
+set_interval(saveData, int(config["frec"]))
 
 client.connect(config["server"])
 client.subscribe("application/" + config["app"] + "/device/" + config["device"] + "/event/up")
