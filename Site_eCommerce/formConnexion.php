87e919cc7515a090e23e3511f1c586c0c8979351
Ozouka <95127761+Ozouka@users.noreply.github.com>
2023-01-22 20:22:37 +0100

    Ajout recuperation mdp

--- Site_eCommerce/formConnexion.php
+++ Site_eCommerce/formConnexion.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href ="styleLogin.css">
+	<link rel="icon" href="./images/LogoOffMA64x64.png">
     <title>Magic Alfombra</title>
   </head>
   <body>
@@ -21,14 +22,16 @@
 
 	<br><br>
 	<br><br>
-
+<div class="contenu">
 	<div class="box">
-		
+			
 		<legend>Identification</legend>
 		
 		<br>
 
 		<p>Login : 
+
+		<br>
 		
 		<?php
 			if (!empty($_COOKIE['nomLogin'])) {
@@ -37,16 +40,43 @@
 			} else {
 				echo "<input type='text' name='login'><br>";
 			}
+			if(isset($_GET['pIndex'])){
+				if ($_GET['pIndex'] == 'Panier'){
+					setcookie('pPanier',$_GET['pIndex'],time()+5);
+				}
+			}
+
+			if(isset($_GET['pDetail'])){
+				if ($_GET['pDetail'] == 'oui'){
+					setcookie('cookieDetail',$_GET['pDetail'],time()+3600);
+				}
+			}
+
 			?>
 			
-		<br> <br>
-		Mot de passe : <input type="password" name="motPasse"><br><br>
-		Se souvenir de moi : <input type="checkbox" name="cb_souvenirMoi"><br><br>
-		<input type="submit" name="Valider" value="Valider">
+		<br>
+		Mot de passe : 
+		
+		<br>
+		
+		<input type="password" name="motPasse">
+
+		<br><br>
+
+		<a href = "mdpOublie.php"> Mot de passe oublié ?</a>
+
+		<br><br>
+
+		Se souvenir de moi : <input type="checkbox" name="cb_souvenirMoi">
+		
+		<br><br>
+
+		<input class="valid-button" type="submit" name="Valider" value="Valider">
 		</p>
 		<br>
 		Pas encore de compte ? <a href = "creatCompte.php"> Cliquez ici</a>
 		
+	</div>
 	</div>
 
 	</form>
